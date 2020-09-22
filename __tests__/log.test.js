f556c79614e7b1b4584524ad4861608566d825e8
SarahRector <rector.sarah.r@gmail.com>
2020-09-22 16:00:08 -0700

    added put, update and delete routes

--- __tests__/log.test.js
+++ __tests__/log.test.js
@@ -38,9 +38,65 @@ describe('log routes', () => {
     return request(app)
       .get('/api/v1/logs')
       .then(res => {
+        // eslint-disable-next-line no-unused-vars
         logs.forEach(log => {
           expect(res.body).toEqual(expect.arrayContaining(logs));
         });
       });
+  });
+
+  it('updates a log by id via PUT', async() => {
+    const log = await Log.insert({
+      recipeId: 123,
+      dateOfEvent: 'March 8th',
+      notes: 'sub-par',
+      rating: 2
+    });
+
+    return request(app)
+      .put(`/api/v1/logs/${log.id}`)
+      .send({
+        recipeId: 888,
+        dateOfEvent: 'September 8th',
+        notes: 'exquisite',
+        rating: 10
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          recipeId: 888,
+          dateOfEvent: 'September 8th',
+          notes: 'exquisite',
+          rating: 10
+        });
+      });
+  });
+
+  it('finds a log by id via GET', async() => {
+    const log = await Log.insert({
+      recipeId: 123,
+      dateOfEvent: 'March 8th',
+      notes: 'sub-par',
+      rating: 2
+    });
+
+    const response = await request(app)
+      .get(`/api/v1/logs/${log.id}`);
+
+    expect(response.body).toEqual(log);
+  });
+
+  it('deletes a log via DELETE', async() => {
+    const log = await Log.insert({
+      recipeId: 123,
+      dateOfEvent: 'March 8th',
+      notes: 'sub-par',
+      rating: 2
+    });
+
+    const response = await request(app)
+      .delete(`/api/v1/logs/${log.id}`);
+
+    expect(response.body).toEqual(log);
   });
 });
